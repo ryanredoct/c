a5e287feb5f413da7fe1a76efc2304b9407a2bde
ryanredoct <154858225+ryanredoct@users.noreply.github.com>
2024-04-27 17:56:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,18 @@
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const express = require('express');
-const mysql = require('mysql');
-const port = process.env.PORT || 8080;
-const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const port = process.env.PORT||8080
+const app = express()
 
-// Database connection
-const db = mysql.createConnection({
-  host: 'database-1.cfe82au48n6b.us-west-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'Man123red',
-  database: 'database-1'
-});
-
-
+app.use(bodyParser.json() , cors())
+ 
+ 
 app.get('/', (req, res) => {
-  res.send('Welcome to Nodejs API Project');
-});
+   res.send('Welcome to Nodejs API Project')
+ })
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!!');
-});
+ app.get('/hello', (req, res) => {
+   res.send('Hello World!!')
+ })
 
-// Endpoint to add name and age
-app.post('/addPerson', (req, res) => {
-  const sql = 'INSERT INTO people SET ?';
-  const newPerson = { name: req.body.name, age: req.body.age };
-  db.query(sql, newPerson, (err, result) => {
-    if (err) {
-      console.error('Failed to add person:', err);
-      res.status(500).send('Error adding person');
-      return;
-    }
-    res.send('Person added');
-  });
-});
-
-app.listen(port, () => console.log(`Server is up and running on port ${port}`));
-
+app.listen(port, () =>  console.log(`server is up and running ${port}`))
