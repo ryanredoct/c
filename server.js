a27e556363815a9e03ff822f24d2a10fcc5ade12
ryanredoct <154858225+ryanredoct@users.noreply.github.com>
2024-04-27 17:14:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-{
-  "name": "your-app",
-  "version": "1.0.0",
-  "main": "app.js",
-  "scripts": {
-    "start": "node app.js"
-  },
-  "dependencies": {
-    "express": "^4.17.1"
-  }
-}
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const port = process.env.PORT||8080
+const app = express()
+
+app.use(bodyParser.json() , cors())
+ 
+ 
+app.get('/', (req, res) => {
+   res.send('Welcome to Nodejs API Project')
+ })
+
+ app.get('/hello', (req, res) => {
+   res.send('Hello World!!')
+ })
+
+app.listen(port, () =>  console.log(`server is up and running ${port}`))
